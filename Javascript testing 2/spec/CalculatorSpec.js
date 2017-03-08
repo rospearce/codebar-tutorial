9b57d9f853a73de429fd672835d1d5a459ca22cb
Rosamund Pearce <Multimedia@Multimedias-MacBook-Pro.local>
2017-03-08 20:07:43 +0000

    create javascript calculator test

---
+++ Javascript testing 2/spec/CalculatorSpec.js
@@ -0,0 +1,9 @@
+describe("Cooking Ingredients Converter", function() {
+    describe("Multiply", function() {
+      it("Multiply calculator", function() {
+        var result = multiply(100);
+
+        expect(result).toEqual(300);
+      });
+	 });
+});
---
+++ Javascript testing 2/src/Calculator.js
@@ -0,0 +1,6 @@
+var number2 = 3
+
+function multiply (number) {
+	var multiplyAnswer = number*number2;
+	return multiplyAnswer;
+}
